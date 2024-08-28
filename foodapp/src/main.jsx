91190f64f43419cf99f74bef1664991db2a5a0eb
adam denton <adamdenton50@gmail.com>
2024-08-28 16:32:39 -0500

    update and delete router

--- foodapp/src/main.jsx
+++ foodapp/src/main.jsx
@@ -1,31 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MyRecipes from "./components/pages/MyRecipes";
-import OurFavorites from "./components/pages/OurFavorites";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/home",
-    element: <App />,
-  },
-  {
-    path: "/myrecipes",
-    element: <MyRecipes />,
-  },
-  {
-    path: "/ourfavorites",
-    element: <OurFavorites />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
